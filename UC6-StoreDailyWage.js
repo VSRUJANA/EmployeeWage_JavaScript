91008d779a9702a54dbe8fe3b2a0ece9fc8ae343
VSRUJANA <srujana.valavala@gmail.com>
2020-11-18 23:46:35 +0530

    [srujana] Add. Store Daily wage in an array along with Total wage

--- UC6-StoreDailyWage.js
+++ UC6-StoreDailyWage.js
@@ -1,11 +1,10 @@
-// UC5 - Calculating Wages till a condition of total working hours of 160 or max days of 20 is reached for a month.
+// UC6 - Store Daily wage along with Total wage
+
+// Get working hours of the employee
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-let totalEmpHours = 0;
-let totalWorkingDays = 0;
 
 function getWorkingHrs(empCheck)
 {
@@ -19,14 +18,21 @@ function getWorkingHrs(empCheck)
     }
 }
 
+// Store Daily wage in 'dailyWages' array along with Total wage
 const MAX_HOURS_IN_MONTHS = 160;
 const MAX_NUMBER_OF_DAYS = 20;
+const WAGE_PER_HOUR = 20;
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+let dailyWages = [];
 
 while (totalEmpHours < MAX_HOURS_IN_MONTHS && totalWorkingDays < MAX_NUMBER_OF_DAYS) 
 {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHours += getWorkingHrs(empCheck);
+    let dailyEmpHours = getWorkingHrs(empCheck);
+    totalEmpHours += dailyEmpHours;
+    dailyWages.push(dailyEmpHours * WAGE_PER_HOUR);
 }
 let totalEmpWage = WAGE_PER_HOUR * totalEmpHours;
 console.log("Total working days  : " + totalWorkingDays + "\nTotal working hours : " + totalEmpHours + " \nTotal employee wage : " + totalEmpWage);
